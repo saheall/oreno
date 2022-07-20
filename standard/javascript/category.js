50f7c28871a6a885dd066612ffe60008ea39562e
Shingo Yamada <saheall1976@gmail.com>
2022-07-20 16:58:06 +0900

    「…」修正

--- standard/javascript/category.js
+++ standard/javascript/category.js
@@ -1,4 +1,4 @@
 if ($('.current').eq(0).text() != '4') {
-	$('.btn-prev').closest('li').after('<div class="pgtt">｡ﾄ</div>');
-	$('.btn-next').closest('li').before('<div class="pgtt">｡ﾄ</div>');
+	$('.btn-prev').closest('li').after('<div class="pgtt">…</div>');
+	$('.btn-next').closest('li').before('<div class="pgtt">…</div>');
 }
